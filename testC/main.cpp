30d3e1cb221a438e4b1e4cadf704eefcd2a15cb0
吴梦飞 <Murphy@wmfs-MacBook-Pro.local>
2015-09-14 21:52:41 +0800

    Initial Commit

---
+++ testC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testC
+//
+//  Created by 吴梦飞 on 15/9/14.
+//  Copyright (c) 2015年 吴梦飞. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
